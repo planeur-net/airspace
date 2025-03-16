629a644f315d2c321ca0dba01b9be43a61325412
Ludovic Launer <ludovic.launer@gmail.com>
2025-03-16 20:02:00 +0100

    Update readme automatically

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ Ce fichier est compilé bénévolement à partir des publications [AIP](https://
 Les [Zones de Sensibilité Majeur](https://www.stac.aviation-civile.gouv.fr/fr/zsm) sont mises a jour régulièrement a partir du fichier KML et de l'outil [Kml2OpenAir](https://github.com/llauner/kml2OpenAir)
 
 ### OpenAir Standard / Extended
-Le format OpenAir extended n’étant pas encore correctement supporté dans XCsoar ([#1340](https://github.com/XCSoar/XCSoar/issues/1340), [#1349](https://github.com/XCSoar/XCSoar/pull/1349)) ou dans SeeYou Navigator, une version OpenAir Standard est générée automatiquement a partir de la version maintenue au format extended.
+Le format OpenAir extended n’étant pas encore correctement supporté dans XCsoar ([#1340](https://github.com/XCSoar/XCSoar/issues/1340), [#1349](https://githubcom/XCSoar/XCSoar/pull/1349)) ou dans SeeYou Navigator, une version OpenAir Standard est générée automatiquement a partir de la version maintenue au format extended
 
 
 # Download
 | Fichier | Format | Gen. Auto. | Download |
 | --- | --- | --- | --- |
-| france.txt | OpenAir | :pencil2: | [https://planeur-net.github.io/airspace/france.txt](https://planeur-net.github.io/airspace/france.txt) |
-| france_openair_standard.txt | OpenAir | :heavy_check_mark: | [france_openair_standard.txt](https://planeur-net.github.io/airspace/france_openair_standard.txt) |
-| france.cub | cub | :heavy_check_mark: | [france.cub](https://planeur-net.github.io/airspace/france.cub) |
+| france.txt | OpenAir | :pencil2: | [https://planeur-net.github.io/airspace/france.txt](https://planeur-net.github.io/airspace/france.txt) <br> [https://planeur-net.github.io/airspace/france#2025-03-11T19-28-45.txt](https://planeur-net.githubio/airspace/france#2025-03-11T19-28-45.txt)|
+| france_openair_standard.txt | OpenAir | :heavy_check_mark: | [france_openair_standard.txt](https://planeur-net.github.io/airspace/france_openair_standard.txt) <br> [france_openair_standard#2025-03-11T19-28-45.txt](https://planeur-net.githubio/airspace/france_openair_standard#2025-03-11T19-28-45.txt)|
+| france.cub | cub | :heavy_check_mark: | [france.cub](https://planeur-net.github.io/airspace/france.cub) <br> [france#2025-03-11T19-28-45.cub](https://planeur-net.githubio/airspace/france#2025-03-11T19-28-45.cub) |
 | france.geojson | geojson | :heavy_check_mark: | [france.geojson](https://planeur-net.github.io/airspace/france.geojson) |
--- create_copies.sh
+++ create_copies.sh
@@ -1,45 +1,70 @@
 #!/bin/bash
 
-# Define an array with two file names
-files=("./france.txt" "./france.cub")
+echo "## Create copies with date"
+          cd ./airspace
 
+          # Define an array with two file names
+          files=("./france.txt" "./france.cub" "./france_openair_standard.txt")
 
-# Delete old files
-for file in "${files[@]}"; do
-  if [ -f "$file" ]; then
-    # Get the file name without the directory path
-    filename=$(basename "$file")
-   
-    # Delete existing files with the same suffix before the "_" character
-    suffix="${filename%.*}"
-    rm -f ${suffix}_*
+          # Delete old files
+          for file in "${files[@]}"; do
+            if [ -f "$file" ]; then
+              # Get the file name without the directory path
+              filename=$(basename "$file")
+            
+              # Delete existing files with the same suffix before the "#" character
+              suffix="${filename%.*}"
+              rm -f ${suffix}#*
 
-  else
-    echo "Delete old files: File not found: $file"
-  fi
-done
+            else
+              echo "Delete old files: File not found: $file"
+            fi
+          done
 
-# Get the current date and time in yyyy_mm_dd_hh format
-# Extract the first line of the file
-first_line=$(head -n 1 france.txt)
-# Retrieve the 3rd string separated by a space
-date_version=$(echo $first_line | awk '{print $3}')
-# Replace the ':' character with '-'
-date_version=$(echo $date_version | sed 's/:/-/g')
-# Delete everything after, and including the '+' character
-date_version=$(echo $date_version | sed 's/\+.*//')
+          # Get the current date and time in yyyy_mm_dd_hh format
+          # Extract the first line of the file
+          first_line=$(head -n 1 france.txt)
+          # Retrieve the 3rd string separated by a space
+          date_version=$(echo $first_line | awk '{print $3}')
+          # Replace the ':' character with '-'
+          date_version=$(echo $date_version | sed 's/:/-/g')
+          # Delete everything after, and including the '+' character
+          date_version=$(echo $date_version | sed 's/\+.*//')
 
-# Loop through all provided files
-for file in "${files[@]}"; do
-  if [ -f "$file" ]; then
-    # Get the file name without the directory path
-    filename=$(basename "$file")
-    # Create the new file name
-    new_filename="${filename%.*}_$date_version.${filename##*.}"
-    # Copy the file and rename it
-    cp "$file" "$new_filename"
-    echo "Copied and renamed $file to $new_filename"
-  else
-    echo "File not found: $file"
-  fi
-done
+          # Loop through all provided files
+          for file in "${files[@]}"; do
+            if [ -f "$file" ]; then
+              # Get the file name without the directory path
+              filename=$(basename "$file")
+              # Create the new file name
+              new_filename="${filename%.*}#$date_version.${filename##*.}"
+              # Copy the file and rename it
+              cp "$file" "$new_filename"
+              echo "Copied and renamed $file to $new_filename"
+            else
+              echo "File not found: $file"
+            fi
+          done
+
+          # Update the README.md file
+          readme_file="README.md"
+          if [ -f "$readme_file" ]; then
+            for file in "${files[@]}"; do
+              # Print the file name
+              echo "Update Readme.md: Processing $file"
+              filename=$(basename "$file")
+              suffix="${filename%.*}"
+              
+              # In  all strings containing suffix between, and not including "(" and the next ")", replace the portion between "#" and "." with the date version
+              sed -i "s/\($suffix[^()]*#\)[^\.]*/\1$date_version/g" README.md
+              # In  all strings containing suffix between, and not including "[" and the next "]", replace the portion between "#" and "." with the date version
+              sed -i "s/\($suffix[^][]*#\)[^\.]*/\1$date_version/g" README.md
+              
+              
+            done
+            echo "Updated $readme_file with new date version."
+          else
+            echo "README.md file not found!"
+          fi
+
+       
